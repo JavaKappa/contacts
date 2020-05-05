e25bb84c4d22ddb9ef35f123b340eab8a4f0ea13
JavaKappa <nextnation7@gmail.com>
2020-05-05 17:25:44 +0300

    Initial commit

---
+++ src/main/java/com/kappa/contactmanager/MvcController.java
@@ -0,0 +1,19 @@
+package com.kappa.contactmanager;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * Капу пк
+ * 04.05.2020
+ */
+@Controller
+public class MvcController {
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public String printHello(Model model) {
+        return "index";
+    }
+}
